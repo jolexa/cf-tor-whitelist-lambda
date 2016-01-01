c644242f97cf6db6c2e67004d8fce6b8acde34fa
Jeremy Olexa <jolexa@jolexa.net>
2016-01-01 22:25:28 +0000

    Initial commit to build zip package

---
+++ build-package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DIR=$(mktemp -d)
+cd $DIR
+
+# Install awscli python lib + deps in buildroot
+pip install requests -t .
+# Fetch aws cli wrapper from github
+wget https://raw.githubusercontent.com/jolexa/cloudflare-tor-whitelister/master/cfwhitelist/whitelist.py
+# Fetch the main script from this repo
+wget https://raw.githubusercontent.com/jolexa/cf-tor-whitelist-lambda/master/main.py
+
+# Have to hardcode the bucket name for secrets. TODO, this is not ideal but it
+# works for now
+sed -e -i "s/BUCKETNAME/$1/" main.py
+
+# create zip
+zip -r9 /tmp/cf-tor-whitelist-lambda.zip *
+cd ..
+# cleanup
+rm -rf $DIR
